2e62a3d42dc1f8942f40836ce762f23587a6c0ea
bruce.wang <www6v@126.com>
2020-04-08 18:30:41 +0800

    fix

---
+++ unitTest/demo_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+	//"strconv"
+)
+
+func TestFirstTry(t *testing.T) {
+	t.Log("My first try!")
+	t.Log(Monday, Tuesday)
+	//var i = strconv.Itoa(1)
+}
+
+const(
+	Monday =  1
+	Tuesday = 2
+	Wednesday = 3
+)
+
+const(
+	Readable =  1<<2
+	Writeable
+	Execable
+)
+
+func TestFirstTry1(t *testing.T) {
+	t.Log(Readable)
+}
