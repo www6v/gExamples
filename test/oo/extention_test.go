4376ce4aeb478bab75ea1785e768d119706b2b6f
bruce.wang <www6v@126.com>
2020-04-10 11:24:47 +0800

    fix

---
+++ test/oo/extention_test.go
@@ -0,0 +1,52 @@
+package oo
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+}
+
+func (p * Pet) Speak() {
+	fmt.Print("...")
+}
+
+func (p *Pet) SpeakTo(host string) {
+	p.Speak()
+	fmt.Println(" ", host)
+}
+
+//type Dog struct {
+//	p *Pet
+//}
+//
+//func (d * Dog) Speak() {
+//   fmt.Println("Wang")
+//}
+//
+//func (d *Dog) SpeakTo(host string) {
+//	d.Speak()
+//	fmt.Println(" ", host)
+//}
+//
+//func TestDog(t *testing.T) {
+//	dog:= new(Dog)
+//	dog.SpeakTo("Chao")
+//}
+
+type Dog struct {
+	Pet
+}
+
+func (p * Dog) Speak() {
+	fmt.Print("Wang")
+}
+
+func TestDog(t *testing.T) {
+	//var dog Pet := new(Dog) /// compile fail // 不支持显示类型转换， 不支持继承
+	dog := new(Dog)
+	dog.SpeakTo("Chao")  /// 不支持重载， 不支持LSP
+}
+
+
--- test/oo/interface_test.go
+++ test/oo/interface_test.go
@@ -1,4 +1,4 @@
-package foo
+package oo
 
 import "testing"
 
