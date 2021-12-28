fd175172e4dea64bbf25c705892db198367ff51c
wangwei18 <wangwei18@tuhu.cn>
2021-12-28 10:47:15 +0800

    struct

--- core/function.go
+++ core/function.go
@@ -1,6 +1,6 @@
 package core
 
-/// return 函数类型
+/// 函数是一等公民, return 函数类型
 func setup(task string) func() {
 	println("do some setup stuff for", task)
 	return func() { println("do some teardown stuff for", task) }
--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 }
 
 func coreMethod() {
+	core.StructTest()
 	core.ArrayTest()
 	core.CopyTest()
 	core.MakeTest()
