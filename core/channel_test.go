2b41d262ff53a29e69baa96892dd2fe10da74b51
wangwei18 <wangwei18@tuhu.cn>
2022-01-12 17:18:53 +0800

    channel

--- core/channel_test.go
+++ core/channel_test.go
@@ -2,7 +2,10 @@ package core
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"reflect"
+	"syscall"
 	"testing"
 	"time"
 )
@@ -89,6 +92,8 @@ func newWorker(id int, ch chan Token, nextCh chan Token) {
 		nextCh <- token
 	}
 }
+
+/// 数据传递
 func TestPipeline1(t *testing.T) {
 	chs := []chan Token{make(chan Token), make(chan Token), make(chan Token), make(chan Token)}
 
@@ -99,4 +104,44 @@ func TestPipeline1(t *testing.T) {
 	chs[0] <- struct{}{}
 	select {}
 
+}
+
+/// 信号通知
+func TestSignalNotify(t *testing.T) {
+	var closing = make(chan struct{})
+	var closed = make(chan struct{})
+
+	go func() {
+		// 模拟业务处理
+		for {
+			select {
+			case <-closing:
+				return
+			default:
+				// ....... 业务计算
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
+	// 处理CTRL+C等中断信号
+	termChan := make(chan os.Signal)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM)
+	<-termChan
+
+	close(closing)
+	// 执行退出之前的清理动作
+	go doCleanup(closed)
+
+	select {
+	case <-closed:
+	case <-time.After(time.Second):
+		fmt.Println("清理超时，不等了")
+	}
+	fmt.Println("优雅退出")
+}
+
+func doCleanup(closed chan struct{}) {
+	time.Sleep((time.Minute))
+	close(closed)
 }
