cfa22bd2b3a0f6fbd3b21b70fcf7db083ecfdb56
wangwei18 <wangwei18@tuhu.cn>
2022-01-12 18:29:45 +0800

    channel

--- core/channel_test.go
+++ core/channel_test.go
@@ -145,3 +145,125 @@ func doCleanup(closed chan struct{}) {
 	time.Sleep((time.Minute))
 	close(closed)
 }
+
+/// 任务编排 -> Or-Done 模式
+/**
+这是有一个任务的情况，如果有多个任务，只要有任意一个任务执行完，我们就想获得这
+个信号，这就是 Or-Done 模式。
+比如，你发送同一个请求到多个微服务节点，只要任意一个微服务节点返回结果，就算成
+功，这个时候，就可以参考下面的实现：
+*/
+func TestOrDone(t *testing.T) {
+	start := time.Now()
+	<-or(
+		sig(10*time.Second),
+		sig(20*time.Second),
+		sig(30*time.Second),
+		sig(40*time.Second),
+		sig(50*time.Second),
+		sig(01*time.Minute),
+	)
+	fmt.Printf("done after %v", time.Since(start))
+}
+func sig(after time.Duration) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		time.Sleep(after)
+	}()
+	return c
+}
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	// 特殊情况，只有零个或者1个chan
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+		switch len(channels) {
+		case 2: // 2个也是一种特殊情况
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+
+		default: //超过两个，二分法递归处理
+			m := len(channels) / 2
+			select {
+			case <-or(channels[:m]...):
+			case <-or(channels[m:]...):
+			}
+		}
+	}()
+	return orDone
+}
+
+/// 任务编排 ->  扇入模式
+func fanInRec(chans ...<-chan interface{}) <-chan interface{} {
+	switch len(chans) {
+	case 0:
+		c := make(chan interface{})
+		close(c)
+		return c
+	case 1:
+		return chans[0]
+	case 2:
+		return mergeTwo(chans[0], chans[1])
+	default:
+		m := len(chans) / 2
+		return mergeTwo(
+			fanInRec(chans[:m]...),
+			fanInRec(chans[m:]...))
+	}
+}
+func mergeTwo(a, b <-chan interface{}) <-chan interface{} {
+	c := make(chan interface{})
+	go func() {
+		defer close(c)
+		for a != nil || b != nil { //只要还有可读的chan
+			select {
+			case v, ok := <-a:
+				if !ok { // a 已关闭，设置为nil
+					a = nil
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok { // b 已关闭，设置为nil
+					b = nil
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
+}
+
+/// 任务编排 ->  扇出模式
+func fanOut(ch <-chan interface{}, out []chan interface{}, async bool) {
+	go func() {
+		defer func() { //退出时关闭所有的输出chan
+			for i := 0; i < len(out); i++ {
+				close(out[i])
+			}
+		}()
+		for v := range ch { // 从输入chan中读取数据
+			v := v
+			for i := 0; i < len(out); i++ {
+				i := i
+				if async { //异步
+					go func() {
+						out[i] <- v // 放入到输出chan中,异步方式
+					}()
+				} else {
+					out[i] <- v // 放入到输出chan中，同步方式
+				}
+			}
+		}
+	}()
+}
