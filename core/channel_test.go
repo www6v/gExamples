91c4fed28a49c20b00b9d8a689cb4b42c6d57d3f
wangwei18 <wangwei18@tuhu.cn>
2022-01-12 15:07:05 +0800

    channel

--- core/channel_test.go
+++ core/channel_test.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -42,4 +43,38 @@ func TestChannelTest(t *testing.T) {
 		// return false
 		fmt.Println(false)
 	}
+}
+
+///
+func TestChannelWithReflect(t *testing.T) {
+	var ch1 = make(chan int, 10)
+	var ch2 = make(chan int, 10)
+	var cases = createCases(ch1, ch2)
+	for i := 0; i < 10; i++ {
+		chosen, recv, ok := reflect.Select(cases)
+		if recv.IsValid() { // recv case
+			fmt.Println("recv:", cases[chosen].Dir, recv, ok)
+		} else { // send case
+			fmt.Println("send:", cases[chosen].Dir, ok)
+		}
+	}
+}
+
+func createCases(chs ...chan int) []reflect.SelectCase {
+	var cases []reflect.SelectCase
+	for _, ch := range chs {
+		cases = append(cases, reflect.SelectCase{
+			Dir:  reflect.SelectRecv,
+			Chan: reflect.ValueOf(ch),
+		})
+	}
+	for i, ch := range chs {
+		v := reflect.ValueOf(i)
+		cases = append(cases, reflect.SelectCase{
+			Dir:  reflect.SelectSend,
+			Chan: reflect.ValueOf(ch),
+			Send: v,
+		})
+	}
+	return cases
 }
