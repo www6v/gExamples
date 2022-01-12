a1e9a261ff8ad99d453ad60405135132ba742128
wangwei18 <wangwei18@tuhu.cn>
2022-01-12 15:26:31 +0800

    channel && pipeline

--- core/channel_test.go
+++ core/channel_test.go
@@ -78,3 +78,25 @@ func createCases(chs ...chan int) []reflect.SelectCase {
 	}
 	return cases
 }
+
+type Token struct{}
+
+func newWorker(id int, ch chan Token, nextCh chan Token) {
+	for {
+		token := <-ch
+		fmt.Println((id + 1))
+		time.Sleep(time.Second)
+		nextCh <- token
+	}
+}
+func TestPipeline1(t *testing.T) {
+	chs := []chan Token{make(chan Token), make(chan Token), make(chan Token), make(chan Token)}
+
+	for i := 0; i < 4; i++ {
+		go newWorker(i, chs[i], chs[(i+1)%4])
+	}
+
+	chs[0] <- struct{}{}
+	select {}
+
+}
