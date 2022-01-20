75c5df5edbc7e62da07f89030946ad522f467cba
wangwei18 <wangwei18@tuhu.cn>
2022-01-20 11:41:19 +0800

    channel

--- core/channel_test.go
+++ core/channel_test.go
@@ -267,3 +267,14 @@ func fanOut(ch <-chan interface{}, out []chan interface{}, async bool) {
 		}
 	}()
 }
+
+func TestChannelType() {
+	ch1 := make(chan<- int, 1)
+	ch2 := make(<-chan int, 1)
+	// <-ch1     // invalid operation: <-ch1 (receive from send-only type chan<- in
+	// ch2 <- 13 // invalid operation: ch2 <- 13 (send to receive-only type <-chan
+	ch1 <- 17
+	a := <-ch2
+
+	fmt.Print(a)
+}
