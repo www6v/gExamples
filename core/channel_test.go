f65266ff9ee3d717e0c9b9f542e90631a75d5d37
wangwei18 <wangwei18@tuhu.cn>
2022-01-20 12:07:01 +0800

    channel

--- core/channel_test.go
+++ core/channel_test.go
@@ -48,7 +48,7 @@ func TestChannelTest(t *testing.T) {
 	}
 }
 
-///
+/// << 14 | Channel：透过代码看典型的应用模式 >>  晁岳攀
 func TestChannelWithReflect(t *testing.T) {
 	var ch1 = make(chan int, 10)
 	var ch2 = make(chan int, 10)
@@ -266,15 +266,4 @@ func fanOut(ch <-chan interface{}, out []chan interface{}, async bool) {
 			}
 		}
 	}()
-}
-
-func TestChannelType() {
-	ch1 := make(chan<- int, 1)
-	ch2 := make(<-chan int, 1)
-	// <-ch1     // invalid operation: <-ch1 (receive from send-only type chan<- in
-	// ch2 <- 13 // invalid operation: ch2 <- 13 (send to receive-only type <-chan
-	ch1 <- 17
-	a := <-ch2
-
-	fmt.Print(a)
 }
---
+++ core/channel_test1.go
@@ -0,0 +1,15 @@
+package core
+
+import "fmt"
+
+// 33｜并发：小channel中蕴含大智慧 - Tony Bai
+func TestChannelType() {
+	ch1 := make(chan<- int, 1)
+	ch2 := make(<-chan int, 1)
+	// <-ch1     // invalid operation: <-ch1 (receive from send-only type chan<- in
+	// ch2 <- 13 // invalid operation: ch2 <- 13 (send to receive-only type <-chan
+	ch1 <- 17
+	a := <-ch2
+
+	fmt.Print(a)
+}
